8d9ab886b4d6062a5862f97277e3463d5a2d023e
Moheshwar Amarnath Biswas <amarnathnew82376@outlook.com>
2022-03-17 09:04:40 +0600

    Added comments.

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+# import dos commands
 import os
+# Creates a variable (loop)
 loop = 0
+# Gets the text and store it in variable (text)
 text = input("What text do you wanna print?\n")
+# Gets how many times and store it in variable (times)
 times = int(input("How many times\n"))
 
-for loop in range(0, times):
+# Runs the loop and print the text.
+for loop in range(0,times):
     print(text)
+
+# Print pakte and pauses the console
 print("Press any key to exit...")
-os.system('pause > nul')
+os.system("pause > nul")
